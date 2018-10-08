6575c3b8ff0c760def5e3e2523577f4f680c064c
Sofyan Sugianto <sofyanartem@gmail.com>
2018-10-08 23:31:16 +0700

    add new config for DB

---
+++ application/config/form_validation.php
@@ -0,0 +1,9 @@
+<?php
+/*
+/--------------------------------------------------------------------------
+/ Config error message on form validation
+/--------------------------------------------------------------------------
+*/
+$config['error_prefix'] = '<div class="error_prefix">';
+$config['error_suffix'] = '</div>';
+?>
